11187b4da65e4672072f10d46411399e605aaaed
deerformera <forestinelf@gmail.com>
2024-03-04 14:42:36 +0700

    cactus double shoot

--- characters/cactus/cactus.gd
+++ characters/cactus/cactus.gd
@@ -6,11 +6,14 @@ onready var cat = Global.get_cat()
 func entered(body):
 	$EnterArea/CollisionShape2D.set_deferred("disabled", true)
 	animstate.travel("attack")
-	
 
 func attack():
+	shoot()
+	yield(get_tree().create_timer(0.1), "timeout")
+	shoot()
+
+func shoot():
 	var bullet_ins = bullet.instance()
 	bullet_ins.dir = (cat.global_position - (self.global_position + Vector2(0, -20))).normalized()
-	bullet_ins.position.y -= 10
+	bullet_ins.position.y -= 20
 	add_child(bullet_ins)
-#	call_deferred("add_child", bullet_ins)
