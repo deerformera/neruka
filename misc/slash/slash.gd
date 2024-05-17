317d3db8865df2788a4ddd67dc9e39177bd0e844
hmadrois <ahmadroisul412@gmail.com>
2024-05-17 14:53:56 +0700

    hit effect

--- misc/slash/slash.gd
+++ misc/slash/slash.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var hitEffect = preload("res://misc/resources/hitEffect.tscn")
+
 func _ready():
 	self.connect("body_entered", self, "onHit")
 	$AnimatedSprite.connect("animation_finished", self, "finished")
@@ -7,8 +9,16 @@ func _ready():
 
 func onHit(body: Character):
 	var cat = Utils.get_cat()
+	if cat == null: return
 	body.damaged(cat.damage)
-	if cat: cat.hit()
+	cat.hit()
+	
+	var hit_ins = hitEffect.instance()
+	var middle = (body.global_position - cat.global_position)
+	hit_ins.rotation = middle.angle()
+	hit_ins.global_position = cat.global_position + middle / 1.5
+	get_tree().root.get_node("World").add_child(hit_ins)
+	
 
 func finished():
 	self.queue_free()
