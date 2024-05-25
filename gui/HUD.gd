9e318fcffba4bfa17039c069cc19e41def3ed1ed
hmadrois <ahmadroisul412@gmail.com>
2024-05-25 09:47:38 +0700

    boss healthbar

--- gui/HUD.gd
+++ gui/HUD.gd
@@ -11,16 +11,20 @@ func pressed():
 	Tab.open("inventory")
 
 func bossEnter(boss: Boss):
-	$M/C/BossHealthbar.show()
+	$M/BossHealthbar.show()
 	if boss: 
 		self.boss = boss
-		$M/C/BossHealthbar.max_value = boss.health * 10
+		$M/BossHealthbar/Background.max_value = boss.health
+		$M/BossHealthbar/Foreground.max_value = boss.health
 
 func bossExit():
-	$M/C/BossHealthbar.hide()
+	$M/BossHealthbar.hide()
 	self.boss = null
 
 func _physics_process(delta):
-	if boss: $M/C/BossHealthbar.value = boss.health * 10
+	if boss: 
+		$M/BossHealthbar/Foreground.value = boss.health
+		$M/BossHealthbar/Background.value = lerp($M/BossHealthbar/Background.value, $M/BossHealthbar/Foreground.value, 0.2)
+	
 	$M/Top/CatHealthbar/Foreground.value = Utils.get_cat().health
 	$M/Top/CatHealthbar/Background.value = lerp($M/Top/CatHealthbar/Background.value, $M/Top/CatHealthbar/Foreground.value, 0.2)
