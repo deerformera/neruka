f39d887213c8aa0009cab416ab5bb81d30b886b1
hmadrois <ahmadroisul412@gmail.com>
2024-05-31 05:36:38 +0700

    rabid rabbit completion

--- characters/rabidRabbit/rabidRabbit.gd
+++ characters/rabidRabbit/rabidRabbit.gd
@@ -1,1 +1,11 @@
 extends Boss
+
+func _ready():
+	$AttackArea.connect("body_entered", self, "onAttack")
+
+func onAttack(body):
+	body.emit_signal("damaged", self.damage)
+
+func die():
+	.die()
+	$StateMachine/Aggressive.timer.stop()
--- characters/rabidRabbit/states/aggressive.gd
+++ characters/rabidRabbit/states/aggressive.gd
@@ -1,7 +1,7 @@
 extends State
 
 var velocity: Vector2
-var timer = Utils.create_timer(1, false, false)
+var timer = Utils.create_timer(1.5, false, false)
 
 func _ready():
 	add_child(timer)
@@ -14,13 +14,15 @@ func update():
 	if owner.get_node("WallRay").is_colliding():
 		velocity = Vector2()
 
-
 func hop():
 	velocity = (owner.target.global_position - owner.global_position).normalized() * 200
 	owner.get_node("WallRay").look_at(owner.target.global_position)
 	owner.get_node("AnimationTree").set("parameters/Hop/blend_position", velocity)
 	owner.get_node("AnimationTree").set("parameters/Idle/blend_position", velocity)
+	owner.get_node("AttackArea/CollisionShape2D").set_deferred("disabled", false)
+	owner.get_node("AttackArea").look_at(owner.target.global_position)
 	
 	owner.animstate.travel("Hop")
 	yield(get_tree().create_timer(0.5), "timeout")
 	velocity = Vector2()
+	owner.get_node("AttackArea/CollisionShape2D").set_deferred("disabled", true)
